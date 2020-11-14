3969282f95acebbd2c9eb5dac856b316a5efffef
eduardo3g <eduardosbrasil10@gmail.com>
2020-11-14 12:36:47 -0300

    test: implement post confirmation user signup

--- __tests__/test_cases/integration/confirm-user-signup.tests.js
+++ __tests__/test_cases/integration/confirm-user-signup.tests.js
@@ -13,8 +13,19 @@ describe('When confirmUserSignUp runs', () => {
 
     const ddbUser = await TouchEvent.user_exists_in_UsersTable(username);
 
-    expect(ddbUser).toEqual({
-
+    expect(ddbUser).toMatchObject({
+      id: username,
+      name,
+      createdAt: expect.stringMatching(/\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d(?:\.\d+)?Z?/g),
+      followersCount: 0,
+      followingCount: 0,
+      tweetsCount: 0,
+      likesCount: 0,
     });
+
+    const [firstName, lastName] = name.split(' ');
+
+    expect(ddbUser.screenName).toContain(firstName);
+    expect(ddbUser.screenName).toContain(lastName);
   });
 });
