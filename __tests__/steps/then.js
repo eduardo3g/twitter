dcc6e0601eeffcc8716a29c8beb40a368fdeb437
eduardo3g <eduardosbrasil10@gmail.com>
2020-11-14 12:36:26 -0300

    test: add then step function

---
+++ __tests__/steps/then.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const AWS = require('aws-sdk');
+
+const user_exists_in_UsersTable = async (id) => {
+  const DynamoDB = new AWS.DynamoDB.DocumentClient();
+
+  console.log(`looking for ser [${id}] in table [${process.env.USERS_TABLE}]`);
+
+  const response = await DynamoDB.get({
+    TableName: process.env.USERS_TABLE,
+    Key: {
+      id,
+    },
+  }).promise();
+
+  expect(response.Item).toBeTruthy();
+
+  return response.Item;
+};
+
+module.exports = {
+  user_exists_in_UsersTable,
+};
