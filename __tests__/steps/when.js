406dcdc2b0f7a234c47f6d77b409be55010faf34
eduardo3g <eduardosbrasil10@gmail.com>
2020-11-17 10:57:11 -0300

    test: add confirmUserSignUp end-to-end test

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const AWS = require('aws-sdk');
 
 const we_invoke_confirmUserSignUp = async (username, name, email) => {
   const handler = require('../../functions/confirm-user-signup').handler;
@@ -27,6 +28,36 @@ const we_invoke_confirmUserSignUp = async (username, name, email) => {
   await handler(event, context);
 };
 
+const a_users_signsup = async (password, name, email) => {
+  const cognito = new AWS.CognitoIdentityServiceProvider();
+
+  const userPoolId = process.env.COGNITO_USER_POOL_ID;
+  const clientId = process.env.WEB_COGNITO_USER_POOL_CLIENT_ID;
+
+  const signUpResponse = await cognito.signUp({
+    ClientId: clientId,
+    Username: email,
+    Password: password,
+    UserAttributes: [
+      { Name: 'name', Value: name },
+    ],
+  }).promise();
+
+  const username = signUpResponse.UserSub;
+
+  await cognito.adminConfirmSignUp({
+    UserPoolId: userPoolId,
+    Username: username,
+  }).promise();
+
+  return {
+    username,
+    name,
+    email,
+  };
+};
+
 module.exports = {
   we_invoke_confirmUserSignUp,
+  a_users_signsup,
 };
