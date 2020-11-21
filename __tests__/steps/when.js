2cadfe3c019ffabd082f9661862a9604abdbdb40
eduardo3g <eduardosbrasil10@gmail.com>
2020-11-20 21:38:03 -0300

    test: add e2e test for getMyProfile query

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -3,6 +3,7 @@ const AWS = require('aws-sdk');
 const fs = require('fs');
 const velocityMapper = require('amplify-appsync-simulator/lib/velocity/value-mapper/mapper');
 const velocityTemplate = require('amplify-velocity-template');
+const GraphQL = require('../lib/graphql');
 
 const we_invoke_confirmUserSignUp = async (username, name, email) => {
   const handler = require('../../functions/confirm-user-signup').handler;
@@ -71,8 +72,38 @@ const we_invoke_an_appsync_template = (templatePath, context) => {
   return JSON.parse(compiler.render(context));
 };
 
+const a_user_calls_getMyProfile = async (user) => {
+  const getMyProfile = `query MyQuery {
+    getMyProfile {
+      website
+      tweetsCount
+      screenName
+      name
+      location
+      likesCount
+      imageUrl
+      id
+      followingCount
+      followersCount
+      createdAt
+      birthdate
+      bio
+      backgroundImageUrl
+    }
+  }`;
+
+  const data = await GraphQL(process.env.API_URL, getMyProfile, {}, user.accessToken);
+
+  const profile = data.getMyProfile;
+
+  console.log(`${user.username} - fetched profile`);
+
+  return profile;
+};
+
 module.exports = {
   we_invoke_confirmUserSignUp,
   a_users_signsup,
   we_invoke_an_appsync_template,
+  a_user_calls_getMyProfile,
 };
