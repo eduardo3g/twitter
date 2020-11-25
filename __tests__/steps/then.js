f794e28ef1bc3525c6979fb3307a83399f139d51
eduardo3g <eduardosbrasil10@gmail.com>
2020-11-25 18:02:24 -0300

    test: add two then steps to simulate when a user upload image to url and download it from url

--- __tests__/steps/then.js
+++ __tests__/steps/then.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const AWS = require('aws-sdk');
+const http = require('axios');
+const fs = require('fs');
 
 const user_exists_in_UsersTable = async (id) => {
   const DynamoDB = new AWS.DynamoDB.DocumentClient();
@@ -16,6 +18,31 @@ const user_exists_in_UsersTable = async (id) => {
   return response.Item;
 };
 
+const user_can_upolad_image_to_url = async (url, filePath, contentType) => {
+  const data = fs.readFileSync(filePath);
+
+  await http({
+    method: 'put',
+    url,
+    headers: {
+      'Content-Type': contentType,
+    },
+    data,
+  });
+
+  console.log('uploaded image to', url);
+};
+
+const user_can_download_image_from = async (url) => {
+  const response = await http(url);
+
+  console.log('downloaded image from', url);
+
+  return response.data;
+};
+
 module.exports = {
   user_exists_in_UsersTable,
+  user_can_upolad_image_to_url,
+  user_can_download_image_from,
 };
