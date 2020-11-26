016fd771ad5cd2706f663251421115a7ef2caf5b
eduardo3g <eduardosbrasil10@gmail.com>
2020-11-26 12:25:28 -0300

    test: add unit test to validate __typename as 'OtherProfile' when current user gets other users tweets

--- __tests__/test_cases/unit/Tweet.profile.response.js
+++ __tests__/test_cases/unit/Tweet.profile.response.js
@@ -5,7 +5,11 @@ const path = require('path');
 
 describe('Tweet.profile.response template', () => {
   it("Should set __typename as 'MyProfile' for current user", () => {
-    const templatePath = path.resolve(__dirname, '../../../mapping-templates/Tweet.profile.response.vtl');
+    const templatePath = path.resolve(
+      __dirname,
+      '../../../mapping-templates/Tweet.profile.response.vtl'
+    );
+
     const username = chance.guid();
     const context = given.an_appsync_context({ username }, {}, { id: username });
     const result = when.we_invoke_an_appsync_template(templatePath, context);
@@ -13,6 +17,23 @@ describe('Tweet.profile.response template', () => {
     expect(result).toEqual({
       id: username,
       __typename: 'MyProfile'
+    });
+  });
+
+  it("Should set __typename as 'OtherProfile' for other users", () => {
+    const templatePath = path.resolve(
+      __dirname,
+      '../../../mapping-templates/Tweet.profile.response.vtl'
+    );
+
+    const username = chance.guid();
+    const id = chance.guid();
+    const context = given.an_appsync_context({ username }, {}, { id });
+    const result = when.we_invoke_an_appsync_template(templatePath, context);
+
+    expect(result).toEqual({
+      id,
+      __typename: 'OtherProfile'
     });
   });
 });
