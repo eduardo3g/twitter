4ba77b195fb98b29976aab72bf71a4d6a40e0c9b
eduardo3g <eduardosbrasil10@gmail.com>
2020-11-26 12:22:00 -0300

    fix: add map function to velocityMapper on appsync template 'when' step

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -100,7 +100,7 @@ const we_invoke_an_appsync_template = (templatePath, context) => {
   const template = fs.readFileSync(templatePath, { encoding: 'utf-8' });
   const ast = velocityTemplate.parse(template);
   const compiler = new velocityTemplate.Compile(ast, {
-    valueMapper: velocityMapper,
+    valueMapper: velocityMapper.map,
     escape: false,
   });
 
