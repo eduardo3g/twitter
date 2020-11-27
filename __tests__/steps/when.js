ac83bace58030b7e936d0fb1ad1594a6ecf5b517
eduardo3g <eduardosbrasil10@gmail.com>
2020-11-27 19:01:32 -0300

    test: add acceptance test for getMyTimeline query

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -252,6 +252,42 @@ const a_user_calls_getTweets = async (user, userId, limit, nextToken) => {
   return tweets;
 };
 
+const a_user_calls_getMyTimeline = async (user, limit, nextToken) => {
+  const getMyTimeline = `query getMyTimeline($limit: Int!, $nextToken: String) {
+    getMyTimeline(limit: $limit, nextToken: $nextToken) {
+      nextToken
+      tweets {
+        id
+        createdAt
+        profile {
+          id
+          name
+          screenName
+        }
+
+        ... on Tweet {
+          text
+          replies
+          likes
+          retweets
+        }
+      }
+    }
+  }`;
+
+  const variables = {
+    limit,
+    nextToken,
+  };
+
+  const data = await GraphQL(process.env.API_URL, getMyTimeline, variables, user.accessToken);
+  const result = data.getMyTimeline;
+
+  console.log(`${user.username} - fetched timeline`);
+
+  return result;
+};
+
 module.exports = {
   we_invoke_confirmUserSignUp,
   we_invoke_getImageUploadUrl,
@@ -263,4 +299,5 @@ module.exports = {
   a_user_calls_getImageUploadUrl,
   a_user_calls_tweet,
   a_user_calls_getTweets,
+  a_user_calls_getMyTimeline,
 };
--- __tests__/test_cases/e2e/tweeting.tests.js
+++ __tests__/test_cases/e2e/tweeting.tests.js
@@ -58,5 +58,28 @@ describe('Given an authentication user', () => {
           });
       });
     });
+
+    describe('When he calls getMyTimeline', () => {
+      let tweets, nextToken;
+      beforeAll(async () => {
+        const result = await when.a_user_calls_getMyTimeline(user, 25);
+        tweets = result.tweets;
+        nextToken = result.nextToken;
+      });
+
+      it('He will see the new tweet in the tweets array', () => {  
+        expect(nextToken).toBeNull();
+        expect(tweets.length).toEqual(1);
+        expect(tweets[0]).toMatchObject(tweet);
+      });
+  
+      it('He cannot request more than 25 tweets in a page', async () => {
+        await expect(when.a_user_calls_getMyTimeline(user, 26))
+          .rejects
+          .toMatchObject({
+            message: expect.stringContaining('max limit is 25'),
+          });
+      });
+    });
   });
 });
