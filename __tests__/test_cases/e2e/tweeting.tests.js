e0d44bb013bef69756da354ac16da7b3479b654b
eduardo3g <eduardosbrasil10@gmail.com>
2020-11-27 18:42:33 -0300

    refactor: separate tweeting acceptance tests into a describe block

--- __tests__/test_cases/e2e/tweeting.tests.js
+++ __tests__/test_cases/e2e/tweeting.tests.js
@@ -27,28 +27,36 @@ describe('Given an authentication user', () => {
       });
     });
 
-    it('He will see the new tweet when he calls getTweets', async () => {
-      const { tweets, nextToken } = await when.a_user_calls_getTweets(
-        user,
-        user.username,
-        25
-      );
+    describe('When he calls get tweets', () => {
+      let tweets, nextToken;
+      beforeAll(async () => {
+        const result = await when.a_user_calls_getTweets(
+          user,
+          user.username,
+          25
+        );
 
-      expect(nextToken).toBeNull();
-      expect(tweets.length).toEqual(1);
-      expect(tweets[0]).toMatchObject(tweet);
-    });
+        tweets = result.tweets;
+        nextToken = result.nextToken;
+      });
 
-    it('He cannot request more than 25 tweets in a page', async () => {
-      await expect(when.a_user_calls_getTweets(
-        user,
-        user.username,
-        26
-      ))
-        .rejects
-        .toMatchObject({
-          message: expect.stringContaining('max limit is 25'),
-        });
+      it('He will see the new tweet in the tweets array', () => {  
+        expect(nextToken).toBeNull();
+        expect(tweets.length).toEqual(1);
+        expect(tweets[0]).toMatchObject(tweet);
+      });
+  
+      it('He cannot request more than 25 tweets in a page', async () => {
+        await expect(when.a_user_calls_getTweets(
+          user,
+          user.username,
+          26
+        ))
+          .rejects
+          .toMatchObject({
+            message: expect.stringContaining('max limit is 25'),
+          });
+      });
     });
   });
 });
