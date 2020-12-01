948320c753619e16f7b019794ed638c523e120e1
eduardo3g <eduardosbrasil10@gmail.com>
2020-12-01 20:13:28 -0300

    feat: ad GraphQL fragment to graphql client on tests folder

--- __tests__/lib/graphql.js
+++ __tests__/lib/graphql.js
@@ -1,47 +1,66 @@
 const http = require('axios');
 const _ = require('lodash');
 
-const throwOnErrors = ({ query, variables, errors }) => {
+const fragments = {};
+const registerFragment = (name, fragment) => fragments[name] = fragment;
+
+const throwOnErrors = ({query, variables, errors}) => {
   if (errors) {
     const errorMessage = `
 query: ${query.substr(0, 100)}
-
-variables: ${JSON.stringify(variables, null, 2)}
-
+  
+variales: ${JSON.stringify(variables, null, 2)}
+  
 error: ${JSON.stringify(errors, null, 2)}
-    `;
-
+`;
     throw new Error(errorMessage);
   }
-};
+}
 
-module.exports = async (url, query, variables = {}, auth) => {
+function* findUsedFragments (query, usedFragments = new Set()) {
+  for (const name of Object.keys(fragments)) {
+    if (query.includes(name) && !usedFragments.has(name)) {
+      usedFragments.add(name);
+      yield name;
+
+      const fragment = fragments[name];
+      const nestedFragments = findUsedFragments(fragment, usedFragments);
+
+      for (const nestedName of Array.from(nestedFragments)) {
+        yield nestedName;
+      }
+    }
+  }
+}
+  
+module.exports.registerFragment = registerFragment;
+module.exports.GraphQL = async (url, query, variables = {}, auth) => {
   const headers = {};
-
   if (auth) {
     headers.Authorization = auth;
   }
 
+  const usedFragments = Array
+    .from(findUsedFragments(query))
+    .map(name => fragments[name]);
+
   try {
-    const response = await http({
+    const resp = await http({
       method: 'post',
       url,
       headers,
       data: {
-        query,
+        query: [query, ...usedFragments].join('\n'),
         variables: JSON.stringify(variables),
       },
     });
-  
-    const { data, errors } = response.data;
-  
-    throwOnErrors({ query, variables, errors });
-  
+
+    const { data, errors } = resp.data;
+    throwOnErrors({query, variables, errors});
     return data;
   } catch (error) {
     const errors = _.get(error, 'response.data.errors');
-    throwOnErrors({ query, variables, errors });
-
+    throwOnErrors({query, variables, errors});
     throw error;
   }
-};
+}
