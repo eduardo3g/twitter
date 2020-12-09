f0ea98ae03d8f81b3540a88c398654c28dca8974
eduardo3g <eduardosbrasil10@gmail.com>
2020-12-08 22:01:01 -0300

    test: add we_invoke_retweet 'when' step

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -146,6 +146,23 @@ const we_invoke_tweet = async (username, text) => {
   return await handler(event, context);
 };
 
+const we_invoke_retweet = async (username, tweetId) => {
+  const handler = require('../../functions/retweet').handler;
+
+  const context = {};
+
+  const event = {
+    identity: {
+      username
+    },
+    arguments: {
+      tweetId,
+    },
+  };
+
+  return await handler(event, context);
+};
+
 const a_users_signsup = async (password, name, email) => {
   const cognito = new AWS.CognitoIdentityServiceProvider();
 
@@ -363,6 +380,7 @@ module.exports = {
   we_invoke_confirmUserSignUp,
   we_invoke_getImageUploadUrl,
   we_invoke_tweet,
+  we_invoke_retweet,
   a_users_signsup,
   we_invoke_an_appsync_template,
   a_user_calls_getMyProfile,
