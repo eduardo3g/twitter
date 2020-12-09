05a8f190187fafb395f268a741ea912ecf462308
eduardo3g <eduardosbrasil10@gmail.com>
2020-12-08 22:08:16 -0300

    test: add integration tests to validate when a user retweets his own tweets

---
+++ __tests__/test_cases/integration/retweet.tests.js
@@ -0,0 +1,44 @@
+const given = require('../../steps/given');
+const when = require('../../steps/when');
+const then = require('../../steps/then');
+const chance = require('chance').Chance();
+
+describe('Given an authenticated user with a tweet', () => {
+  let userA, tweet;
+  const text = chance.string({ length: 16 });
+
+  beforeAll(async () => {
+    userA = await given.an_authenticated_user();
+    tweet = await when.we_invoke_tweet(userA.username, text);
+  });
+
+  describe('When he retweets his own tweet', () => {
+    beforeAll(async () => {
+      await when.we_invoke_retweet(userA.username, tweet.id);
+    });
+
+    it('Should save the retweet in the Tweets table', async () => {
+      await then.retweet_exists_in_TweetsTable(userA.username, tweet.id);
+    });
+
+    it('Should save the retweet in the Retweets table', async () => {
+      await then.retweet_exists_in_RetweetsTable(userA.username, tweet.id);
+    });
+
+    it('Should increment the retweets count in the Tweets table', async () => {
+      const { retweets } = await then.tweet_exists_in_TweetsTable(tweet.id);
+
+      expect(retweets).toEqual(1);
+    });
+
+    it('Should increment the tweetsCount in the Users table', async () => {
+      await then.tweetsCount_is_updated_in_UsersTable(userA.username, 2);
+    });
+
+    it('Should NOT save the retweet in the Timelines table', async () => {
+      const tweets = await then.there_are_N_tweets_in_TimelinesTable(userA.username, 1);
+
+      expect(tweets[0].tweetId).toEqual(tweet.id);
+    });
+  });
+});
