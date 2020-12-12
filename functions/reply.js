5521644abaa41828797d12d905a23b50698b7023
eduardo3g <eduardosbrasil10@gmail.com>
2020-12-12 18:20:14 -0300

    feat: implement reply function

---
+++ functions/reply.js
@@ -0,0 +1,105 @@
+const _ = require('lodash');
+const DynamoDB = require('aws-sdk/clients/dynamodb');
+const DocumentClient = new DynamoDB.DocumentClient();
+const ulid = require('ulid');
+const { TweetTypes } = require('../lib/constants');
+const { getTweetById } = require('../lib/tweets');
+
+const {
+  USERS_TABLE,
+  TWEETS_TABLE,
+  TIMELINES_TABLE,
+} = process.env;
+
+module.exports.handler = async (event) => {
+  const { tweetId, text } = event.arguments;
+  const { username } = event.identity;
+  const id = ulid.ulid();
+  const timestamp = new Date().toJSON();
+
+  const tweet = await getTweetById(tweetId);
+
+  if (!tweet) {
+    throw new Error('Tweet was not found');
+  }
+
+  const inReplyToUserIds = await getUserIdsToReplyTo(tweet);
+
+  const newRetweet = {
+    __typename: TweetTypes.REPLY,
+    id,
+    creator: username,
+    createdAt: timestamp,
+    inReplyToTweetId: tweetId,
+    inReplyToUserIds,
+    text,
+    replies: 0,
+    likes: 0,
+    retweets: 0,
+  };
+
+  // Save a new reply in the TWEETS_TABLE
+  // Save a new entry in the RETWEETS_TABLE
+  // Increment the retweets count of the original tweet by one
+  // Increment the tweets count of the user by one
+  const transactItems = [{
+    Put: {
+      TableName: TWEETS_TABLE,
+      Item: newRetweet,
+    },
+  }, {
+    Update: {
+      TableName: TWEETS_TABLE,
+      Key: {
+        id: tweetId,
+      },
+      UpdateExpression: 'ADD replies :one',
+      ExpressionAttributeValues: {
+        ':one': 1,
+      },
+      ConditionExpression: 'attribute_exists(id)',
+    },
+  }, {
+    Update: {
+      TableName: USERS_TABLE,
+      Key: {
+        id: username,
+      },
+      UpdateExpression: 'ADD tweetsCount :one',
+      ExpressionAttributeValues: {
+        ':one': 1,
+      },
+      ConditionExpression: 'attribute_exists(id)',
+    },
+  }, {
+    Put: {
+      TableName: TIMELINES_TABLE,
+      Item: {
+        userId: username,
+        tweetId: id,
+        timestamp,
+        inReplyToTweetId: tweetId,
+        inReplyToUserIds,
+      },
+    },
+  }];
+
+  await DocumentClient.transactWrite({
+    TransactItems: transactItems 
+  }).promise();
+
+  return true;
+};
+
+async function getUserIdsToReplyTo (tweet) {
+  let userIds = [tweet.creator];
+
+  if (tweet.__typename === TweetTypes.REPLY) {
+    userIds = userIds.concat(tweet.inReplyToUserIds);
+  } else if (tweet.__typename === TweetTypes.RETWEET) {
+    const retweetOf = await getTweetById(tweet.retweetOf);
+    userIds = userIds.concat(await getUserIdsToReplyTo(retweetOf));
+  }
+
+  return _.uniq(userIds);
+}
