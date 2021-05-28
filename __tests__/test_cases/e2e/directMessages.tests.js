a5d72a87116cb6901d513a6df24667c9ce89ebd1
eduardo3g <eduardosbrasil10@gmail.com>
2021-05-28 19:05:24 -0300

    test: add end-to-end acceptance tests to the Direct Messaging context when user B sends a DM to user A

--- __tests__/test_cases/e2e/directMessages.tests.js
+++ __tests__/test_cases/e2e/directMessages.tests.js
@@ -89,6 +89,85 @@ describe('Given two authenticated users', () => {
       });
     });
 
-    
+    describe('When user B sends a DM to user A', () => {
+      let conversation2;
+      const message2 = chance.string({ length: 16 });
+
+      beforeAll(async () => {
+        conversation2 = await when.a_user_calls_sendDirectMessage(
+          userB, 
+          userA.username, 
+          message2
+        );
+      });
+
+      it("The conversation's lastMessage and lastModified fields should be updated", () => {
+        expect(conversation2.lastMessage).toEqual(message2);
+        expect(conversation2.lastModified > conversation.lastModified).toBe(true);
+      });
+
+      it("User A should see the updated conversation when he calls listConversations", async () => {
+        const { conversations, nextToken } = await when.a_user_calls_listConversations(
+          userA,
+          10
+        );
+  
+        expect(nextToken).toBeNull();
+        expect(conversations).toHaveLength(1);
+        expect(conversations[0]).toMatchObject({
+          id: conversation.id,
+          lastMessage: message2,
+          lastModified: conversation2.lastModified,
+          otherUser: {
+            id: userB.username,
+          },
+        });
+      });
+  
+      it("User B should see the updated conversation when he calls listConversations", async () => {
+        const { conversations, nextToken } = await when.a_user_calls_listConversations(
+          userB,
+          10
+        );
+  
+        expect(nextToken).toBeNull();
+        expect(conversations).toHaveLength(1);
+        expect(conversations[0]).toMatchObject(conversation2);
+      });
+
+      it("User A should see the new message when he calls getDirectMessages for the conversation", async () => {
+        const { messages, nextToken } = await when.a_user_calls_getDirectMessages(
+          userA,
+          userB.username,
+          10
+        );
+  
+        expect(nextToken).toBeNull();
+        expect(messages).toHaveLength(2);
+        expect(messages[0]).toMatchObject({
+          from: {
+            id: userB.username
+          },
+          message: message2,
+        });
+      });
+  
+      it("User B should see the message when he calls getDirectMessages for the conversation", async () => {
+        const { messages, nextToken } = await when.a_user_calls_getDirectMessages(
+          userB,
+          userA.username,
+          10
+        );
+  
+        expect(nextToken).toBeNull();
+        expect(messages).toHaveLength(2);
+        expect(messages[0]).toMatchObject({
+          from: {
+            id: userB.username,
+          },
+          message: message2,
+        });
+      });
+    });
   });
 });
